2aeb32f6355954c1a01cd77f784aa3d83b30bc40
darkhowk <darkhowk.dark@gmail.com>
2018-10-02 02:28:25 +0900

    errer fix ( ARMap )

--- src/main/java/admin/comm/controller/CommController.java
+++ src/main/java/admin/comm/controller/CommController.java
@@ -15,7 +15,6 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import admin.comm.service.CommService;
-import comm.util.ARMap;
 
 @Controller
 @RequestMapping("/admin")
